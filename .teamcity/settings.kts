c5220cfe8011601da55a2777962b54cf29622aea
Anton Arhipov <ant.arhipov@gmail.com>
2019-07-03 16:23:56 +0300

    - gradle update - cleanup

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,139 +1,22 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.SvnVcsRoot
 import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2018_2.Project
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.project
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.v2018_2.version
-
-/*
-   sequence    := (stage)+
-   stage    := (serial|parallel)+
-   parallel := (serial|sequence)+
-   serial   := build
- */
 
 version = "2018.2"
 
-//Could do something like this to externalize artifacts from the pipeline
-//val applicationJar = Compiler.artifact("application.jar")
-
 project {
 
-    val sequence = sequence {
-        build(Compile) {
-            produces("application.jar")
-        }
-        parallel {
-            build(Test1) {
-                requires(Compile, "application.jar")
-                produces("test.reports.zip")
-            }
-            sequence {
-                build(Test2) {
-                    requires(Compile, "application.jar")
-                    produces("test.reports.zip")
-                }
-                build(Test3) {
-                    requires(Compile, "application.jar")
-                    produces("test.reports.zip")
-                }
-            }
-        }
-        build(Package) {
-            requires(Compile, "application.jar")
-            produces("application.zip")
-        }
-        build(Publish) {
-            requires(Package, "application.zip")
-        }
-    }
-    println(sequence)
 }
 
-object Compile : BuildType({
-    name = "Compile"
+object MyVcs : GitVcsRoot({
+    name = "MyVcs"
+    url = "http://localhost:3000/anton/app"
 
-    steps {
-        script {
-            scriptContent = "touch application.jar"
-        }
-    }
-
-})
-
-object Test : BuildType({
-    name = "Test"
-
-    steps {
-        script {
-            scriptContent = "touch test.reports.zip"
-        }
-    }
-
-})
-
-object Test1 : BuildType({
-    name = "Test1"
-
-    steps {
-        script {
-            scriptContent = "touch test.reports.zip"
-        }
-    }
-
-})
-
-object Test2 : BuildType({
-    name = "Test2"
-
-    steps {
-        script {
-            scriptContent = "touch test.reports.zip"
-        }
-    }
-})
-
-object Test3 : BuildType({
-    name = "Test3"
-
-    steps {
-        script {
-            scriptContent = "touch test.reports.zip"
-        }
-    }
-})
-
-object Test4 : BuildType({
-    name = "Test4"
-
-    steps {
-        script {
-            scriptContent = "touch test.reports.zip"
-        }
-    }
-})
-
-object Package : BuildType({
-    name = "Package"
-
-    steps {
-        script {
-            scriptContent = "touch application.zip"
-        }
-    }
-})
-
-object Publish : BuildType({
-    name = "Publish"
-
-    steps {
-        script {
-            scriptContent = "echo 'Publish'"
-        }
-    }
-})
-
-object Deploy : BuildType({
-    name = "Deploy"
-
-    type = Type.DEPLOYMENT
+    branchSpec = """
+        +:refs/heads/(master)
+        +:refs/heads/(feature*)
+    """.trimIndent()
 })
--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
-FROM openjdk:8
-CMD "echo 'hello java'"
+FROM openjdk:8-jre
+ADD build/libs/app-0.0.1-SNAPSHOT.jar app.jar
+EXPOSE 8010
+ENTRYPOINT ["java", "-jar","/app.jar"]
